b9e1f341fa975437c1e4c2c8fafa6584c0470fdc
mannuelst <manuelds@outlook.pt>
2024-06-20 16:30:27 +0100

    feat: unit test

---
+++ src/modules/user/test/user.service.spec.ts
@@ -0,0 +1,57 @@
+import { beforeAll, describe, expect, test } from "vitest"
+import { UserService } from "../user.service"
+
+
+
+let useTest: UserService
+
+beforeAll(() => {
+    useTest = new UserService()
+})
+describe('User Service', () => {
+
+    // test("Deve ser possível criar um usuário", () => {
+    //     expect(true).toBeTruthy()
+    // })
+
+
+    test("Deve ser possível criar um usuário", () => {
+        //const userService = new UserService()
+        const result = useTest.create({
+            name: "Manuel dos Santos",
+            username: "mannuelst"
+        })
+        //  console.log(result)
+
+        expect(result).resolves //por ser promisse
+    })
+
+    // onTestFailed // dizendo que o teste irá falhar
+    test("Não deve ser possível criar um usuário", () => {
+        useTest.create({
+            name: "Manuel dos Santos",
+            username: "mannuelst"
+        })
+
+        //  console.log(result)
+
+        //  expect(result).rejects //por ser promisse
+        expect(async () => {
+            await useTest.create({
+                name: "Manuel dos Santos",
+                username: "mannuelst"
+            })
+        }).rejects.toThrow('User already exists')
+
+
+    })
+
+
+    // test('Deve ser possível listar usuários', () => {
+    //     const result = useTest.findAll()
+
+    //     expect(result).toHaveLength(2)
+    // })
+
+})
+

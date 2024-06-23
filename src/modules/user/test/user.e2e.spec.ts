67038cfe2281e56f262fd3a46f131021abe1c7a3
mannuelst <manuelds@outlook.pt>
2024-06-23 17:07:00 +0100

    test: add e2e test

--- src/modules/user/test/user.e2e.spec.ts
+++ src/modules/user/test/user.e2e.spec.ts
@@ -1,17 +1,31 @@
-import { app } from "@src/app"
 import request from "supertest"
 import { describe, test } from "vitest"
+import { app } from "../../../app"
 
 describe('User E2E', () => {
 
-    test("Deve ser possível criar um usuário", async () => {
+    test("Deve ser possível criar um usuário com token", async () => {
+        await app.ready()
+
+        await request(app.server)
+            .post("/users")
+            .set('Authorization', 'Bearer FAKE_TOKEN')
+            .send({
+                name: "User Test E2E",
+                username: "user_name_test_e2e"
+            }).expect(200)
+
+    })
+
+    test("Não deve ser possível criar um usuário sem token", async () => {
         await app.ready()
 
         await request(app.server)
             .post("/users")
             .send({
-                name: "User Test E2E", username: "user_name_test_e2e"
-            }).expect(200)
+                name: "User Test E2E",
+                username: "user_name_test_e2e"
+            }).expect(401)
 
     })
 
